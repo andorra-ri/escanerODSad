a2a6a85b86039753c19c4d37d6f67bfa481c8c53
David Hernandez <davidhernandezruiz@gmail.com>
2020-10-02 17:28:58 +0200

    Adds base code for the scanner.

---
+++ src/api/index.js
@@ -0,0 +1,308 @@
+import config from '@/config';
+import axios from 'axios';
+
+const qs = require("qs");
+
+
+export default {
+  getTopics() {
+    return axios.get(getEndpoint()).then(response => response.data);
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/topics/'
+      ].join('');
+    }
+  },
+  getTopic(topicId) {
+    return axios.get(getEndpoint(topicId)).then(response => response.data);
+
+    function getEndpoint(topicId) {
+      return [
+        config.URL,
+        '/topics/',
+        topicId
+      ].join('');
+    }
+  },
+  getGroups(params) {
+    if (params === undefined) params = {};
+    Object.keys(params).forEach(key => !params[key] && delete params[key]);
+
+    return axios
+      .get(getEndpoint(), {
+        params: params,
+        paramsSerializer: function (params) {
+          return qs.stringify(params)
+        },
+      })
+      .then(response => response.data);
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/parliamentary-groups/'
+      ].join('');
+    }
+  },
+  getGroup(parliamentarygroupId) {
+    return axios.get(getEndpoint(parliamentarygroupId)).then(response => response.data);
+
+    function getEndpoint(parliamentarygroupId) {
+      return [
+        config.URL,
+        '/parliamentary-groups/',
+        parliamentarygroupId
+      ].join('');
+    }
+  },
+  getDeputies(params) {
+    if (params === undefined) params = {};
+    Object.keys(params).forEach(key => !params[key] && delete params[key]);
+
+    return axios
+      .get(getEndpoint(), {
+        params: params,
+        paramsSerializer: function (params) {
+          return qs.stringify(params)
+        },
+      })
+      .then(response => response.data);
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/deputies/'
+      ].join('');
+    }
+  },
+  getDeputy(deputyId) {
+    return axios.get(getEndpoint(deputyId)).then(response => response.data);
+
+    function getEndpoint(deputyId) {
+      return [
+        config.URL,
+        '/deputies/',
+        deputyId
+      ].join('');
+    }
+  },
+  getPlaces() {
+    return axios.get(getEndpoint()).then(response => response.data);
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/places/'
+      ].join('');
+    }
+  },
+  getStatus() {
+    return axios.get(getEndpoint()).then(response => response.data);
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/initiative-status/'
+      ].join('');
+    }
+  },
+  getTypes() {
+    return axios.get(getEndpoint()).then(response => response.data);
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/initiative-types/'
+      ].join('');
+    }
+  },
+  getInitiatives(params) {
+    Object.keys(params).forEach(key => !params[key] && delete params[key]);
+
+    return axios
+      .get(getEndpoint(), {
+        params: params,
+        paramsSerializer: function (params) {
+          return qs.stringify(params, { arrayFormat: 'repeat' })
+        },
+      })
+      .then(response => response.data);
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/initiatives/'
+      ].join('');
+    }
+  },
+  getInitiative(id) {
+    return axios
+      .get(getEndpoint(id))
+      .then(response => response.data);
+
+    function getEndpoint(id) {
+      return [
+        config.URL,
+        '/initiatives/',
+        id
+      ].join('');
+    }
+  },
+  saveAlert(search) {
+    Object.keys(search).forEach(key => !search[key] && delete search[key]);
+
+    return axios
+      .post(getEndpoint(), search)
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/alerts'
+      ].join('');
+    }
+  },
+  getTags(topicId) {
+    return axios
+      .get(getEndpoint(topicId))
+      .then(response => response.data.tags);
+
+    function getEndpoint(topicId) {
+      return [
+        config.URL,
+        '/topics/',
+        topicId
+      ].join('');
+    }
+  },
+  getOverallStats() {
+    return axios
+      .get(getEndpoint())
+      .then(response => response.data);
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/stats/overall'
+      ].join('');
+    }
+  },
+  getDeputiesRanking(topic, subtopic, limit = 5) {
+    let params = {'topic': topic};
+    if (subtopic) {
+      params['subtopic'] = subtopic;
+    }
+    return axios
+      .get(getEndpoint(), {
+        params: params
+      })
+      .then(response => response.data.slice(0,limit));
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/stats/deputies'
+      ].join('');
+    }
+  },
+  getParliamentarygroupsRanking(topic, subtopic) {
+    let params = {'topic': topic};
+    if (subtopic) {
+      params['subtopic'] = subtopic;
+    }
+    return axios
+      .get(getEndpoint(), {
+        params: params
+      })
+      .then(response => response.data.slice(0,5));
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/stats/parliamentarygroups'
+      ].join('');
+    }
+  },
+  getPlacesRanking(topic, subtopic) {
+    let params = {'topic': topic};
+    if (subtopic) {
+      params['subtopic'] = subtopic;
+    }
+    return axios
+      .get(getEndpoint(), {
+        params: params
+      })
+      .then(response => response.data);
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/stats/places'
+      ].join('');
+    }
+  },
+  annotate(text, file) {
+    let formData = new FormData()
+    formData.append('text', text)
+    formData.append('file', file)
+
+    return axios.post(
+      getEndpoint(),
+      formData,
+    )
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/tagger/'
+      ].join('');
+    }
+  },
+  getScannerResult(taskID) {
+    return axios.get(
+      getEndpoint(taskID)
+    )
+
+    function getEndpoint(taskID) {
+      return [
+        config.URL,
+        '/tagger/result/',
+        taskID
+      ].join('');
+    }
+  },
+  saveScanned(title, excerpt, result) {
+    return axios.post(
+      getEndpoint(),
+      {
+        title: title,
+        excerpt: excerpt,
+        result: JSON.stringify(result)
+      }
+    )
+
+    function getEndpoint() {
+      return [
+        config.URL,
+        '/scanned/',
+      ].join('');
+    }
+  },
+  getScanned(scannedId) {
+    return axios.get(
+      getEndpoint(scannedId)
+    ).then(response => response.data)
+      .catch(error => {
+        console.log(error.response)
+      });
+
+    function getEndpoint(scannedId) {
+      return [
+        config.URL,
+        '/scanned/',
+        scannedId
+      ].join('');
+    }
+  }
+};
---
+++ src/components/footer-block.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div id="footer" class="o-container o-container--fluid o-section u-bg-secondary-light">
+
+      <div class="o-container">
+        <div class="o-grid">
+          <div class="o-grid__col u-7@sm u-margin-top-4">
+            <p class="u-text-overline">Impulsado por</p>
+            <div class="o-grid">
+              <div class="o-grid__col u-7@sm">
+                <a href="http://www.exteriores.gob.es/Portal/es/Ministerio/SecretariosDeEstado/SecretariaDeEstadoCooperacionInternacional/Paginas/Inicio.aspx" target="_blank"><img src="/img/maeuec-seci.jpg" class="big" alt="Logo de la Secretaria de Estado de Cooperación (Ministerio de Asuntos Exteriores, Unión Europea y Cooperación) del Gobierno de España"></a>
+              </div>
+              <div class="o-grid__col u-5@sm">
+                <a href="http://politicalwatch.es" target="_blank"><img src="/img/ciecode.png" class="big" alt="Logo de CIECODE"></a>
+              </div>
+            </div>
+          </div>
+          <div class="o-grid__col u-4@sm u-offset-1@sm u-margin-top-4">
+            <p class="u-text-overline">Contacto</p>
+            <tipi-icon icon="github" class="c-icon--secondary-dark" />&nbsp;<a class="u-color-secondary-dark u-text-tbody2" href="http://github.com/politicalwatch/" title="github.com/politicalwatch" target="_blank">Consulta nuestro código en Github</a>
+            <br>
+            <tipi-icon icon="mail" class="c-icon--secondary-dark" />&nbsp;<a class="u-color-secondary-dark u-text-tbody2" href="mailto:parlamento2030@ciecode.es" title="parlamento2030@ciecode.es" target="_blank">Escribe a parlamento2030@ciecode.es</a>
+          </div>
+        </div>
+        <div class="o-grid">
+          <div class="o-grid__col u-12@sm u-margin-top-4">
+            <p class="u-color-secondary-dark u-text-tbody2">{{ (new Date()).getFullYear() }} Political Watch. Algunos derechos reservados.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="c-decorator" style="background-image: url('/img/multicolor.jpg')"></div>
+
+    <cookie-law theme="base" buttonText="Entendido" buttonClass="btn btn-custom" message="Este sitio usa cookies para asegurarte la mejor experiencia web."></cookie-law>
+
+  </div>
+</template>
+
+<script>
+  import CookieLaw from 'vue-cookie-law';
+  import { TipiIcon } from 'tipi-uikit';
+
+  export default {
+    name: "footer-block",
+    components: {
+      CookieLaw,
+      TipiIcon
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+</style>
