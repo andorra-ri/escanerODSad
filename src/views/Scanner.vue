eee8e2c8b130836798519c606308c6832d3c3609
David Hernandez <davidhernandezruiz@gmail.com>
2020-10-07 08:30:25 +0200

    Updates text on the placeholder of the textarea of the scanner.

--- src/views/Scanner.vue
+++ src/views/Scanner.vue
@@ -9,7 +9,7 @@
           </div>
 
           <div class="o-grid__col u-12 u-6@sm">
-            <p><textarea placeholder="Inserta aqui el texto que quieres escanear..." v-model="inputText" rows="9"/></p>
+            <p><textarea placeholder="Pega aquí el texto que quieres escanear..." v-model="inputText" rows="9"/></p>
             <div class="c-input-label c-input-label--file u-block">
               <label for="file">Sube un archivo</label>
               <input type="file" id="file" name="file" v-on:change="loadSelectedFile" placeholder="PDF, doc o txt">
